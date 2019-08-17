f484c29f1fcc887b0761371ecf3ee752725ac57e
Shubhaw Kumar <shukumar@microsoft.com>
2019-08-18 02:11:19 +0530

    171: Using Logo in the application

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styleClasses from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => {
     return (
         <header className={styleClasses.Toolbar}>
             <div>MENU</div>
-            <div>LOGO</div>
+            <Logo />
             <nav>
                 ...
             </nav>
