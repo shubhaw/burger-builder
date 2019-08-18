2dfb9b27d63a7a8545f34e34364dcd4d31dbabbe
Shubhaw Kumar <shukumar@microsoft.com>
2019-08-18 14:19:37 +0530

    176: Reusing the Backdrop. Making the Layout component a stateful component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styleClasses from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={styleClasses.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        isSideDrawerVisible: true
+    }
 
-export default layout;
+    sideDrawerOpenedHandler = () => this.setState({ isSideDrawerVisible: true });
+    sideDrawerClosedHandler = () => this.setState({ isSideDrawerVisible: false });
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    show={this.state.isSideDrawerVisible}
+                    onClosed={this.sideDrawerClosedHandler} />
+                <main className={styleClasses.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import styleClasses from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
+    console.log(props.show)
     return (
-        <div className={styleClasses.SideDrawer}>
-            <div className={styleClasses.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.show} click={props.onClosed} />
+            <div className={[styleClasses.SideDrawer, props.show? styleClasses.Open: styleClasses.Close].join(' ')}>
+                <div className={styleClasses.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
