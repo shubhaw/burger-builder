70287c665fe0b145332291334e616a59138d0321
Shubhaw Kumar <shukumar@microsoft.com>
2019-08-25 22:20:16 +0530

    258: Fetching orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        isLoading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                console.log(res);
+                const fetchedOrders = [];
+                for(let key in res.data) {
+                    fetchedOrders.push( {
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState( {
+                    isLoading: false,
+                    orders: fetchedOrders
+                });
+            })
+            .catch(error => {
+                this.setState( {
+                    isLoading: false
+                });
+            });
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +44,4 @@ class Orders extends Component {
 
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
